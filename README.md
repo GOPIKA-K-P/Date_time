7ca82aca1e47d2573bb4195f29d25fd8c3d6c0d1
Gopika <kamalgopika54@gmail.com>
2023-12-20 15:37:40 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Project
+
+This content is dynamically generated: Wed Dec 20 09:43:53 UTC 2023
---
+++ scripts/update_readme.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set locale and time zone
+export LC_TIME=en_IN.UTF-8
+export TZ=Asia/Kolkata
+
+# Generate dynamic content (current date and time in Indian Standard Time)
+indian_time=$(date +'%Y-%m-%d %H:%M:%S %Z')
+
+# Convert Indian time to UTC
+utc_time=$(date -d "$indian_time" -u +'%Y-%m-%d %H:%M:%S %Z')
+
+# Update README file
+echo "# My Project" > README.md
+echo -e "\nThis content is dynamically generated in Indian Time (IST): $indian_time" >> README.md
+echo -e "Converted to UTC: $utc_time" >> README.md
+
+# Commit and push changes
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git add README.md
+git commit -m "Update README with dynamic content"
+git push
